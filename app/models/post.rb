d811b771501ac0467ec82b3c4d20f308b9612d0c
Quy <quy.nguyenngoctp@gmail.com>
2017-10-03 19:23:13 +0700

    create form for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  SECTIONS = ["Health and Beauty", "Programming", "Book", "Eat and drink", "Other"]
+  enum section: SECTIONS
+  validates :title, :content, :section, presence: true
 end
